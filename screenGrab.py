d446d117d1087dfce14b680b82c7688bc1d664aa
Aaron Bargotta <bargotta@princeton.edu>
2019-06-13 02:42:34 +0100

    Screen grab play area

---
+++ screenGrab.py
@@ -0,0 +1,29 @@
+from PIL import ImageGrab
+import os
+import time
+
+"""
+All coordinates assume a screen resolution of 1440x900, and Chrome 
+sized to 1440x730 with the Bookmarks Toolbar disabled.
+
+x_pad = 584
+y_pad = 264
+Play area =  x_pad+1, y_pad+1, 1582, 897
+"""
+
+# Globals
+# ------------------
+x_pad = 584
+y_pad = 264
+ 
+def screenGrab():
+    box = (x_pad + 1, y_pad + 1, x_pad + 998, y_pad + 633)
+    im = ImageGrab.grab(box)
+    path = os.getcwd() + '/snaps/snap__' + str(int(time.time())) + '.png'
+    im.save(path, 'PNG')
+
+def main():
+    screenGrab()
+ 
+if __name__ == '__main__':
+    main()
